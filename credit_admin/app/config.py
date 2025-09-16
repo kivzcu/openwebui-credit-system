fa99f6b27e2c58a91498c3edfc3f018b75166d7c
Miloslav Konopík <konopoik@kiv.zcu.cz>
2025-09-16 10:47:21 +0200

    minor: Add DATABASE_URL environment variable to configuration for postresql support

--- credit_admin/app/config.py
+++ credit_admin/app/config.py
@@ -6,6 +6,7 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 
 # Database
 DB_FILE = os.getenv("OPENWEBUI_DATABASE_PATH", "/root/.open-webui/webui.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Script paths
 SCRIPT_DIR = os.path.join(BASE_DIR, "app", "data")
